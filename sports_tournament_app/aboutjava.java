74774d9716cc91423e7262b4fadda6fd9e34c004
37299221 <94833856+37299221@users.noreply.github.com>
2023-02-10 13:27:39 +0000

    Added layout, java and manifest file/s

---
+++ sports_tournament_app/aboutjava.java
@@ -0,0 +1,14 @@
+package com.example.sports_tournament_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class aboutjava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.about_page);
+    }
+}
---
+++ sports_tournament_app/helpjava.java
@@ -0,0 +1,15 @@
+package com.example.sports_tournament_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class helpjava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_page);
+    }
+
+}
